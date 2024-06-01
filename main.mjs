763c332b3086f6bd1300d13221c5c7e92190a570
Michael Hunter <elec.mike@gmail.com>
2024-06-01 09:20:51 +0700

    Initial commit

---
+++ main.mjs
@@ -0,0 +1,156 @@
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+// Load environment variables from a .env file
+dotenv.config();
+
+// Set up OpenAI configuration
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+/**
+ * A message from one agent to others.
+ *
+ * Agents always see their own messages presented as "assistant" messages, but
+ * messages from other agents can appear either as "user" or "system" messages.
+ * This allows us to have agents that actually represent the system itself, such
+ * as world-building agents.
+ *
+ * Users always see messages they've sent, but they will only see messages from
+ * other agents if those messages are intended for them. This is specifically
+ * design to allow private system messages.
+ *
+ * @typedef {Object} Message
+ * @property {string} [from] If not specified, the message is not attributed to any agent
+ * @property {string[]} [to] If not specified, the message is visible to all agents
+ * @property {'user' | 'system'} [as] If not specified, the message is sent as a user message
+ * @property {string} content
+ */
+
+/**
+ * @typedef {Object} Scenario
+ * @property {AgentProfile[]} agents Agents who automatically participate in the conversation
+ * @property {Message[]} conversation
+ * @property {number | undefined} temperature
+ * @property {number | undefined} maxTokens
+ */
+
+/**
+ * @typedef {Object} AgentProfile
+ * @property {string} name The name of the agent
+ * @property {'system'|'user'} role The role of the agent
+ * @property {string} systemPrompt System prompt shown at the beginning of the conversation
+ * @property {string} trailingSystemPrompt System prompt shown at the end of the conversation, normally for reminders to the agent about things that get forgotten from the main system prompt over long conversations.
+ */
+
+// Get the scenario filename from the command line arguments
+const inputFilename = process.argv[2];
+
+if (!inputFilename) {
+  console.error('Usage: npm start <scenario.json> [<next speaker>]');
+  process.exit(1);
+}
+
+/** @type {Scenario} */
+const scenario = JSON.parse(fs.readFileSync(inputFilename, 'utf8'));
+
+/** @type {AgentProfile | undefined} */
+let whoseTurn;
+
+// If the user provided the next person to speak, then use that person
+if (process.argv[3]) {
+  whoseTurn = scenario.agents.find(a => a.name.toLowerCase() === process.argv[3].toLowerCase());
+  if (!whoseTurn) {
+    console.error(`No active agent with the name "${process.argv[3]}".`);
+    process.exit(1);
+  }
+  console.log(`It is ${whoseTurn.name}'s turn to speak (specified on CLI).`);
+}
+
+if (!whoseTurn) {
+  // The next person to speak is the agent who last spoke longest ago.
+
+  // How long ago each agent last spoke
+  const lastSpokeTimes = scenario.agents
+    .map(a => scenario.conversation.slice().reverse().findIndex(m => m.from === a.name))
+    .map(i => i === -1 ? Infinity : i);
+  whoseTurn = scenario.agents[lastSpokeTimes.indexOf(Math.max(...lastSpokeTimes))];
+
+  console.log(`It is ${whoseTurn.name}'s turn to speak (it's been the longest since they spoke).`);
+}
+
+// Generate a response from the AI agent
+const response = await generateResponse(
+  scenario.conversation,
+  whoseTurn,
+  scenario.maxTokens,
+  scenario.temperature
+);
+
+scenario.conversation.push({
+  from: whoseTurn.name,
+  as: whoseTurn.role === 'system' ? 'system' : undefined,
+  content: response
+});
+
+// Derive output filename from input filename
+// const outputFilename = inputFilename.replace('.json', '.out.json');
+const outputFilename = inputFilename; // Overwrite the input file with the updated conversation
+
+fs.writeFileSync(outputFilename, JSON.stringify(scenario, null, 2));
+
+// A text rendering for convenience
+const textOutputFile = inputFilename.replace('.json', '.md');
+fs.writeFileSync(textOutputFile, scenario.conversation
+  .map(m => `# ${m.from ?? '<anonymous>'}${
+    !m.to ? '' : ` to ${[m.to].flat().join(', ')}`
+  }${
+    m.as === 'system' ? ' as "system" role' : ''
+  }:\n${m.content}`).join('\n\n'));
+
+/**
+ * Function to generate a response from an AI agent
+ *
+ * @param {Message[]} conversation The conversation so far
+ * @param {AgentProfile} whoseTurn The agent whose turn it is to speak
+ * @param {number | undefined} maxTokens The maximum number of tokens to generate
+ * @param {number | undefined} temperature The temperature to use for generation
+ */
+async function generateResponse(conversation, whoseTurn, maxTokens, temperature) {
+
+  /** @type {{ role: 'system' | 'assistant' | 'user', content: string }[]} */
+  const messages = [];
+
+  if (whoseTurn.systemPrompt) {
+    messages.push({ role: 'system', content: whoseTurn.systemPrompt });
+  }
+
+  for (const { from, to, as, content } of conversation) {
+    const isVisible = whoseTurn.name === from || !to || to.includes(whoseTurn.name);
+    if (isVisible) {
+      // If the message is from this agent then they see it as an assistant
+      // message. Otherwise, they see it as a user or system message depending
+      // on the message's `as` property.
+      const role = whoseTurn.name === from ? 'assistant' : (as ?? 'user');
+      messages.push({ role, content });
+    }
+  }
+
+  if (whoseTurn.trailingSystemPrompt) {
+    messages.push({ role: 'system', content: whoseTurn.trailingSystemPrompt });
+  }
+
+  fs.writeFileSync('messages-dbg.json', JSON.stringify(messages, null, 2));
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-4o',
+    messages,
+    max_tokens: maxTokens ?? 1500,
+    temperature: temperature ?? 0.5,
+  });
+  const content = response.choices[0].message.content ?? '';
+
+  return content.trim();
+}
---
+++ readme.md
@@ -0,0 +1,42 @@
+# Multi-way AI Conversation
+
+CLI app to run multiple AI agents to chat with each other or with you.
+
+## Install
+
+Clone repo and `npm install`.
+
+## Usage
+
+```
+npm start {scenario.json} [{next speaker}]
+```
+
+This will let `{next speaker}` take a turn to speak, and append the new message back to the conversation in the scenario.json file.
+
+If no {next speaker} is specified, the next speaker will be the agent who spoke longest ago, so they will take turns.
+
+The scenario file contains both the setup and conversation. Look at [example-scenario.json](example-scenario.json) for an example of a scenario.
+
+The script only runs a single turn, with the expectation that the user may want to intervene by modifying the conversation themselves before the next turn. You might do this if you want to interact with the agents (add your own messages), shape the conversation by modifying the agent's responses, etc.
+
+There's a rendered `md` file as output as well, but please not that this is not the source of truth. It's just a pretty rendering of the JSON file. If you want to manipulate the conversation, you need to do it in the json file.
+
+
+## Conversation Model
+
+This uses OpenAI's chat API (currently GPT4o). Conversations submitted to the chat API have messages with 3 roles: 'system', 'user', and 'assistant'. System messages are low-level instructions to the agent. User messages are the messages that the agent is responding to. Assistant messages are the messages that the agent itself believes it generated.
+
+In a scenario file, there is a single conversation that all agents see, but they see it through different lenses:
+
+- Each agent sees themselves as "assistant" in the conversation.
+- They see messages from others normally as "user" in the conversation, unless the `as: 'system'` property is added to the message.
+- Agents typically produce messages to others as "user", unless the agent's `role` is set to `system`, in which case their messages will show up to others as `system` messages.
+
+Messages that don't have a `from` property will never be seen by any agent as an "assistant" message. I.e. they will never attribute the message to themselves. This can be used if you want to add your own messages to the conversation and not have any of the agents think that they generated it. Like other messages, you can set the `as` property to `system` if you want the message to be seen as a system message by the agents.
+
+If you want to privately message only some agents, you can set the `to` property to an array of agent names. The message will then only be seen by those agents. This can be used, for example, to give system feedback to an agent without the other agents seeing it. An example is a message saying "Sorry, your last action is illegal, please try again" if an agent breaks the rules, which you may want to direct only at a single agent.
+
+Agents must each also be configured with a `systemPrompt`. This appears to them as the first message in the conversation and is generally used to tell the agent who they are and what they're doing.
+
+Agents can optionally be configured by a `trailingSystemPrompt` which is like the system prompt but is always seen to the agent as the *last* message in the conversation. This can be used to give the agent a hint about what to do next, or to give them a reminder of who they are or of the rules, etc. For example `"You are Bob. It's your turn next."`. I found this was required because in long conversations, the agents can forget who they are.
